39d5586fa02d6c85fcc40c9875c4506eee3848e8
A.R Shagla-McKotch <shagla@gmail.com>
2012-07-06 21:19:20 -0400

    Added Markov Chain implementation. Depracted old random string generator. You can now have multiple input files to build the dictionary used in the Markov Chain. Added option to be able to specify what order you want to use for the Markov Chain. Added command line option for number of threads to use. Run ruby generator.rb to see usage statement.

--- generator.rb
+++ generator.rb
@@ -1,22 +1,57 @@
 require 'thread'
 require 'socket'
 require_relative 'thread-pool.rb'
+require_relative 'markov-chains.rb'
 
-unless ARGV.length == 2
-	puts 'Usage: ruby generator.rb host_address host_ port'
+# Ussage statement and check for proper number of command line arguments
+
+unless ARGV.length > 6
+	puts 'Usage: ruby generator.rb host_address host_ port number_threads order number_of_sentences input_file'
+	puts 'Can have multiple input_files'
 	exit
 end
 
-p = Pool.new(30)
+# initiate all variables
+
+p = Pool.new(ARGV[2].to_i)
 host = ARGV[0] 
 port = ARGV[1]
-
+order = ARGV[3].to_i
+number_sentences = ARGV[4].to_i
 socket = TCPSocket.open(host,port)
-while 1 == 1 do
+
+# shift the command line arguments off the array to be able to use ARGF
+# to read in the file for the dictionary 
+
+5.times {
+	ARGV.shift
+}
+
+# Create a Markov Chain
+
+markov = MarkovChainer.new(order)
+
+# Create a new thread to read in the file to create dictionary 
+
+markov.add_text(ARGF.read)
+
+# Schedule threads from the thread pool to 
+# generate sentences and sent them to the target 
+# listening host
+
+number_sentences.times {
 	p.schedule do
-		socket.puts (1..40).map { (("a".."z").to_a + [" "] * 20)[rand(56)] }.join
+		socket.puts markov.generate_sentence
 	end
-end
+}
+
+	
+# Depracated random sentence code
+#while 1 == 1 do
+#	p.schedule do
+#		socket.puts (1..40).map { (("a".."z").to_a + [" "] * 20)[rand(56)] }.join
+#	end
+#end
 
 at_exit { p.shutdown }
 
